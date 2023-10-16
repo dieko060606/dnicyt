cf02ef5d4f835cc98b1a8be35e86396fc2beb5c5
atahuichi46 <atahuichi46@gmail.com>
2023-10-16 11:41:37 -0400

    Descripción de los cambios NJKKJL

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>DNICYT - INDEX</title>
+  <title>ATAPRO</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
